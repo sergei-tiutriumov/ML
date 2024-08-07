7c31bd79376499003cbe1efb9264f413a8cab169
Serj Che <sergei.tiutriumov@gorfax.ru>
2024-08-07 16:06:08 +0300

    db

--- HLW/app/__init__.py
+++ HLW/app/__init__.py
@@ -5,6 +5,8 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
-from app import routes
+from app import routes, models
---
+++ check_envs.py
@@ -0,0 +1,4 @@
+import os
+
+flask_app = os.getenv('FLASK_APP')
+print(f'FLASK_APP: {flask_app}')
