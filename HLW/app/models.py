f926facb7d0a8685c02f42d36cf78fe0d6be508d
Serj Che <sergei.tiutriumov@gorfax.ru>
2024-08-07 16:22:55 +0300

    venv+db

--- HLW/app/models.py
+++ HLW/app/models.py
@@ -1,15 +1,4 @@
 from app import db
-<<<<<<< HEAD
-
-class User(db.Model):
-    id = db.Column(db.Integer, pimary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
-
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
-=======
 from datetime import datetime
 
 class User(db.Model):
@@ -30,4 +19,3 @@ class Posts(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
->>>>>>> 7c31bd7
